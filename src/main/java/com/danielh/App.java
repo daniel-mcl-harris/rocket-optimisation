a04a297c674b6f4ca820ed1c652b729c57055ae7
Daniel <161537212+danmclh@users.noreply.github.com>
2026-04-16 20:09:14 +1200

    running a simulation for a single rocket and ignoring CG and CP

--- src/main/java/com/danielh/App.java
+++ src/main/java/com/danielh/App.java
@@ -9,111 +9,30 @@ import info.openrocket.core.document.OpenRocketDocument;
 import info.openrocket.core.document.Simulation;
 import info.openrocket.core.file.GeneralRocketLoader;
 import info.openrocket.core.file.GeneralRocketSaver;
-import info.openrocket.core.motor.*;
-import info.openrocket.core.database.motor.MotorDatabase;
-import info.openrocket.core.startup.Application;
 
 public class App {
-        // Global list to store mass and CG data for each component
-        static List<ComponentMassCG> massCGList = new ArrayList<>();
-
-        static class ComponentMassCG {
-            String name;
-            String type;
-            double mass;
-            Double cg;
-            Double length;
-            ComponentMassCG(String name, String type, double mass, Double cg, Double length) {
-                this.name = name;
-                this.type = type;
-                this.mass = mass;
-                this.cg = cg;
-                this.length = length;
-            }
-        }
     public static void main(String[] args) {
-        OpenRocketCore.initialize();
+       OpenRocketCore.initialize();
 
         try {
-            String rocketPath = "C:\\Users\\Daniel\\rocket-app\\libs\\test_rocket.ork";
+            String rocketPath = "libs\\test_rocket.ork";
             
             OpenRocketDocument document = loadRocketDocument(new File(rocketPath));
             if (document == null) return;
             
-
-
             Rocket rocket = document.getRocket();
-
-            // Force recalculation of rocket metadata to ensure CG/CP are up to date
-            try {
-                // Try fireComponentChangeEvent
-                java.lang.reflect.Method fireComponentChange = rocket.getClass().getMethod("fireComponentChangeEvent", java.lang.Object.class);
-                fireComponentChange.invoke(rocket, new Object[] { null });
-            } catch (Exception e1) {
-                try {
-                    java.lang.reflect.Method resetMetadata = rocket.getClass().getMethod("resetMetadata");
-                    resetMetadata.invoke(rocket);
-                } catch (Exception e2) {
-                    // Ignore
-                }
-            }
-
-            // Run a simulation to extract CG and CP from simulation results
-            try {
-                System.out.println("\n--- Extracting CG and CP from simulation results ---");
-                Simulation sim = new Simulation(rocket);
-                sim.simulate();
-                java.lang.reflect.Method getSimulatedData = sim.getClass().getMethod("getSimulatedData");
-                Object simulatedData = getSimulatedData.invoke(sim);
-                if (simulatedData != null) {
-                    double cg = 0.0;
-                    double cp = 0.0;
-                    try {
-                        java.lang.reflect.Method getCG = simulatedData.getClass().getMethod("getCG");
-                        Object cgObj = getCG.invoke(simulatedData);
-                        if (cgObj instanceof Double) {
-                            cg = (Double) cgObj;
-                        }
-                    } catch (Exception e) {
-                        System.out.println("  Could not extract CG from simulation: " + e);
-                    }
-                    try {
-                        java.lang.reflect.Method getCP = simulatedData.getClass().getMethod("getCP");
-                        Object cpObj = getCP.invoke(simulatedData);
-                        if (cpObj instanceof Double) {
-                            cp = (Double) cpObj;
-                        }
-                    } catch (Exception e) {
-                        System.out.println("  Could not extract CP from simulation: " + e);
-                    }
-                    System.out.println("Simulated Center of Mass (CG): " + cg + " m");
-                    System.out.println("Simulated Center of Pressure (CP): " + cp + " m");
-                } else {
-                    System.out.println("Simulation did not return data for CG/CP extraction.");
-                }
-            } catch (Exception e) {
-                System.err.println("Error extracting CG/CP from simulation: " + e);
-            }
+            Simulation sim = new Simulation(rocket);
+            sim.simulate();
 
             // Extract actual mass data from the rocket design
-            System.out.println("\n=== ROCKET MASS DATA ===");
+            System.out.println("\n=== ROCKET COMPONENT DATA ===");
             extractAndDisplayMassData(rocket);
 
-            // Test different nose cone lengths
-            double[] noseConeLengths = {0.20};
-
-            System.out.println("\n=== Testing Different Nose Cone Lengths ===");
-            for (double noseLength : noseConeLengths) {
-                System.out.println("\n--- Testing with Nose Cone Length: " + noseLength + " m ---");
-                setNoseConeLength(rocket, noseLength);
-
-                SimulationResult result = runSimulationAndExtractData(rocket, 
-                    "NoseCone=" + String.format("%.2f", noseLength) + "m");
-                if (result != null) {
-                    result.print();
-                }
+            // Run a single simulation and print results
+            SimulationResult result = runSimulationAndExtractData(rocket);
+            if (result != null) {
+                result.print();
             }
-            
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
@@ -209,8 +128,7 @@ public class App {
                 String indent = "  ".repeat(depth);
                 String componentName = component.getName();
                 String componentType = component.getClass().getSimpleName();
-                // Store in global list
-                massCGList.add(new ComponentMassCG(componentName, componentType, mass, cg, length));
+                
                 if (cg != null) {
                     System.out.print(indent + "-" + componentName + " (" + componentType + "): " +
                         String.format("%.4f", mass) + " kg, CG = " + String.format("%.4f", cg) + " m");
@@ -291,7 +209,7 @@ public class App {
         }
     }
     
-    private static SimulationResult runSimulationAndExtractData(Rocket rocket, String motorDesignation) {
+    private static SimulationResult runSimulationAndExtractData(Rocket rocket) {
         try {
             Simulation simulation = new Simulation(rocket);
             simulation.simulate();
@@ -309,77 +227,15 @@ public class App {
             double maxVelocity = getDoubleFromMethod(simulatedData, "getMaxVelocity");
             double maxAcceleration = getDoubleFromMethod(simulatedData, "getMaxAcceleration");
             double maxMach = getDoubleFromMethod(simulatedData, "getMaxMachNumber");
-            
-            // Extract rocket stability parameters
-            double centerOfMass = extractCenterOfMass(rocket);
-            double centerOfPressure = extractCenterOfPressure(rocket);
-            double safetyMargin = centerOfPressure - centerOfMass;
             double outerDiameter = getRocketOuterDiameter(rocket);
             
-            return new SimulationResult(motorDesignation, apogee, timeToApogee, 
-                        maxVelocity, maxAcceleration, maxMach, centerOfMass, centerOfPressure, safetyMargin, outerDiameter);
+            return new SimulationResult("Default", apogee, timeToApogee, 
+                        maxVelocity, maxAcceleration, maxMach, outerDiameter);
             
         } catch (Exception e) {
             System.err.println("Simulation failed: " + e.getMessage());
             return null;
         }
-    }
-    
-    private static double extractCenterOfMass(Rocket rocket) {
-        // Calculate CG using the global massCGList (weighted average)
-        double totalMass = 0.0;
-        double weightedSum = 0.0;
-        for (ComponentMassCG entry : massCGList) {
-            if (entry.mass > 0.00001 && entry.cg != null) {
-                totalMass += entry.mass;
-                weightedSum += entry.mass * entry.cg;
-            }
-        }
-        if (totalMass > 0.0) {
-            return weightedSum / totalMass;
-        } else {
-            return 0.0;
-        }
-    }
-    
-    
-    private static double extractCenterOfPressure(Rocket rocket) {
-        try {
-            // Try direct method if it exists
-            try {
-                java.lang.reflect.Method getCP = rocket.getClass().getMethod("getNormalForceLocation");
-                Object cpCoord = getCP.invoke(rocket);
-                if (cpCoord != null) {
-                    java.lang.reflect.Field xField = cpCoord.getClass().getDeclaredField("x");
-                    xField.setAccessible(true);
-                    return xField.getDouble(cpCoord);
-                }
-                System.out.print("this works!");
-            } catch (NoSuchMethodException e) {
-                // Method doesn't exist, try alternative
-            }
-            
-            // Try alternative method names
-            String[] methodNames = {"getAerodynamicCenter", "getCenterOfPressure", "getCP"};
-            for (String methodName : methodNames) {
-                try {
-                    java.lang.reflect.Method method = rocket.getClass().getMethod(methodName);
-                    Object cpCoord = method.invoke(rocket);
-                    if (cpCoord != null && cpCoord.getClass().getName().contains("Coordinate")) {
-                        java.lang.reflect.Field xField = cpCoord.getClass().getDeclaredField("x");
-                        xField.setAccessible(true);
-                        return xField.getDouble(cpCoord);
-                    }
-                    System.out.print("this works!");
-                } catch (Exception e) {
-                    // Try next method
-                }
-            }
-            
-        } catch (Exception e) {
-            System.err.println("Error extracting center of pressure: " + e.getMessage());
-        }
-        return 0.0;
     }
     
     private static double getDoubleFromMethod(Object obj, String methodName) {
@@ -422,7 +278,6 @@ public class App {
         
         SimulationResult(String motor, double apogee, double timeToApogee, 
                         double maxVelocity, double maxAcceleration, double maxMach,
-                        double centerOfMass, double centerOfPressure, double safetyMargin,
                         double outerDiameter) {
             this.motor = motor;
             this.apogee = apogee;
@@ -430,9 +285,6 @@ public class App {
             this.maxVelocity = maxVelocity;
             this.maxAcceleration = maxAcceleration;
             this.maxMach = maxMach;
-            this.centerOfMass = centerOfMass;
-            this.centerOfPressure = centerOfPressure;
-            this.safetyMargin = safetyMargin;
             this.outerDiameter = outerDiameter;
         }
         
@@ -444,13 +296,6 @@ public class App {
             System.out.println("  Max Velocity: " + String.format("%.2f", maxVelocity) + " m/s");
             System.out.println("  Max Acceleration: " + String.format("%.2f", maxAcceleration) + " m/s²");
             System.out.println("  Max Mach: " + String.format("%.3f", maxMach));
-            
-            System.out.println("STABILITY:");
-            System.out.println("  Center of Mass: " + String.format("%.3f", centerOfMass) + " m");
-            System.out.println("  Center of Pressure: " + String.format("%.3f", centerOfPressure) + " m");
-            double marginInCalibers = safetyMargin / outerDiameter;
-            System.out.println("  Safety Margin: " + String.format("%.3f", safetyMargin) + " m (" + String.format("%.2f", marginInCalibers) + " calibers)");
-            System.out.println("  Status: " + (marginInCalibers > 1.0 ? "STABLE" : "UNSTABLE"));
         }
         
         @Override
